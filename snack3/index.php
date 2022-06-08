58fe0225d073d6a48ae12cafb932001eece471ef
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-06-08 15:56:59 +0200

    add snack3

---
+++ snack3/index.php
@@ -0,0 +1,87 @@
+<!-- ## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+    ?>
+
+    <ul>
+        <?php 
+            for ($i = 0; $i < count(array_keys($posts)); $i++) {
+                $current_date = array_keys($posts)[$i];
+        ?>
+        <li>
+            <h2>
+                Post del: <?php echo $current_date; ?>
+            </h2>
+        </li>
+        <?php 
+            for ($j = 0; $j < count($posts[$current_date]); $j++) {
+                $current_post = $posts[$current_date][$j];
+        ?>
+        <li>
+            <h4>
+                <?php echo $current_post["title"]; ?> /
+                <?php echo $current_post["author"]; ?> /
+                <?php echo $current_post["text"]; ?>
+            </h4>
+        </li>
+        <?php } ?>
+        <?php } ?>
+    </ul>
+</body>
+</html>
